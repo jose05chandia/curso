8a59c607ec4f748064221f5bc369e7ff2e3e499f
josechandia <jose05chandia@gmail.com>
2022-04-21 16:55:43 -0400

    Fix @Transactional error dependencys

--- src/main/java/com/cursojava/curso/dao/UserDaoImpl.java
+++ src/main/java/com/cursojava/curso/dao/UserDaoImpl.java
@@ -2,9 +2,12 @@ package com.cursojava.curso.dao;
 
 import com.cursojava.curso.models.User;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Repository
+
+@Transactional
 
 public class UserDaoImpl implements UserDao{
 
